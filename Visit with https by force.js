804bfc3ef313d9dc4785b0fbf572044be5f4c01b
wujx3433 <32533580+wujx3433@users.noreply.github.com>
2018-06-09 14:33:52 +0800

    Visit with https by force

---
+++ Visit with https by force.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name         强制使用https
+// @name:en  	Visit with https by force
+// @version      0.1 Beta
+// @description  在includes里添加网站，然后在访问这些网站时就会自动变成https
+// @description:en Type the URL into 'inclide', and it will change 'http'to 'https' when you bisit thede website
+// @author       Care L.
+// @namespace	https://weibo.com/CareKun
+// @grant       GM_xmlhttpRequest
+// @icon        https://tva1.sinaimg.cn/crop.821.0.853.853.180/ed8fdad3jw8esdwbbj7xtj21hc0xce0v.jpg
+// @include		*://weibo.com/*
+// @include		*://acg.tv/*
+// @include		*://*.bilibili.com/*
+// @include		*://www.pixiv.net/*
+// @include		*://www.pixivision.net/*
+// @include		*://static.hdslb.com/*
+// @include		*://github.com/*
+// ==/UserScript==
+/* 
+*@author: 	智能P Care L.
+*@E-mail: 	careluu@foxmail.com
+ */
+
+if (window.location.protocol =="http:"){
+    window.location.href = window.location.href.replace("http","https");
+}
