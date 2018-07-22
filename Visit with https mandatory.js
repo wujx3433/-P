b938defed90102d96be01fccffcad0ecee6324e7
wujx3433 <32533580+wujx3433@users.noreply.github.com>
2018-07-22 09:17:03 +0800

    Update Visit with https mandatory.js

--- Visit with https mandatory.js
+++ Visit with https mandatory.js
@@ -5,12 +5,28 @@
 // @description  在includes里添加网站，然后在访问这些网站时就会自动变成https
 // @description:en Type the URL into 'inclide', and it will change 'http'to 'https' when you Visit the website
 // @author       Care L.
-// @namespace	https://github.com/wujx3433/-P/blob/master/Visit%20with%20https%20mandatory.js
+// @namespace	https://weibo.com/CareKun
 // @grant       GM_xmlhttpRequest
 // @icon        https://tva1.sinaimg.cn/crop.821.0.853.853.180/ed8fdad3jw8esdwbbj7xtj21hc0xce0v.jpg
+// @include		*://weibo.com/*
+// @include		*://acg.tv/*
+// @include		*://*.bilibili.com/*
+// @include		*://www.pixiv.net/*
+// @include		*://www.pixivision.net/*
+// @include		*://static.hdslb.com/*
+// @include		*://github.com/*
+// @include		*://www.acfun.cn/*
+// @connect     i.pximg.net
+// @connect     i1.pixiv.net
+// @connect     i2.pixiv.net
+// @connect     i3.pixiv.net
+// @connect     i4.pixiv.net
+// @connect     i5.pixiv.net
+// @connect     imgaz.pixiv.net
+
 // @exclude     *://*.163.com/*
 // ==/UserScript==
-/*
+/* 
 *@author: 	智能P Care L.
 *@E-mail: 	careluu@foxmail.com
  */
